353c26e5cdf6f98e2d6765b3ad486f40d5f0ad7d
Priyank Parashar <paras20xx@gmail.com>
2023-01-25 20:02:20 +0400

    Added minifyCss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { beautifyCss } from './beautifyCss/beautifyCss.js';
 import { cssToScss } from './cssToScss/cssToScss.js';
+import { minifyCss } from './minifyCss/minifyCss.js';
 
 export {
     beautifyCss,
-    cssToScss
+    cssToScss,
+    minifyCss
 };
---
+++ src/minifyCss/minifyCss.js
@@ -0,0 +1,14 @@
+import { csspretty } from '../3rdparty_customized/csspretty_customized.js';
+
+const minifyCss = function (cssCode = '') {
+    const output = csspretty({
+        mode: 'minify',
+        source: cssCode
+    });
+    // Alternatively, use Yahoo's CSS Min library:
+    //     const output = YAHOO.compressor.cssmin(cssCode);
+
+    return output;
+};
+
+export { minifyCss };
